47d2fefd69a66e72a2c2fdcf27365a811821a97b
ja <ja@getexamready.ai>
2024-12-04 18:49:36 +0800

    ausiostream js udpates

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -4,11 +4,11 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="An impressive and flawless site template that includes various UI elements and countless features, attractive ready-made blocks and rich pages, basically everything you need to create a unique and professional website.">
-  <meta name="keywords" content="bootstrap 5, business, corporate, creative, gulp, marketing, minimal, modern, multipurpose, one page, responsive, saas, sass, seo, startup, html5 template, site template">
+  <meta name="description" content="<?php echo isset($meta_desc) ? $meta_desc : 'Paralegal Recruitment helps aspiring professionals secure roles in Accounting, HR, Business Analysis, Data Analysis, Project Management, and Software Testing. Our platform provides mentorship and real-world project experience, ensuring candidates gain the skills and confidence needed to succeed in their careers.' ; ?>">
+  <meta name="keywords" content="">
   <meta name="author" content="elemis">
-  <title>Paralegal Recruitment - Modern & Multipurpose Bootstrap 5 Template</title>
-  <link rel="shortcut icon" href="<?php echo base_url() ?>assets/img/favicon.png">
+  <title><?php echo isset($meta_tags) ? $meta_tags : 'Paralegal Recruitment | Bridge to Career Success with Mentorship and Real Project Experience' ; ?></title>
+  <!-- <link rel="shortcut icon" href="<?php echo base_url() ?>assets/img/favicon.png"> -->
   <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/plugins.css">
   <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/style.css">
   <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/custom.css">
